5e1c7e1d95c80d9dc91747808ebd9ef74d2544fb
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-11-27 12:35:36 +0400

    Began coding the metadata retreival
    - Using xflr6's 'walk_gdrive.py', the application gets all files in a directory and organizes them into a list of dictionaries
    - The application then exports the metadata into a JSON file in the 'metadata' directory

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from src.config import readConfig, writeConfig
 from src.credentials import refreshCredentials
+from src.metadata import writeMetadata
 
 if os.path.exists("config.env"):
     account_list, client_id, client_secret, category_list, refresh_token, secret_key, tmdb_api_key, environment = readConfig()
@@ -10,3 +11,5 @@ else:
 
 drive, access_token = refreshCredentials(
     "", client_id, client_secret, refresh_token, True)
+
+metadata = writeMetadata(category_list, drive)
---
+++ src/metadata.py
@@ -0,0 +1,34 @@
+import json
+import os
+import re
+import time
+
+import requests
+
+from src.walk import driveWalk
+
+
+def writeMetadata(category_list, drive):
+    placeholder_metadata = []
+    for category in category_list:
+        index = next((i for i, item in enumerate(category_list) if (
+            item["name"] == category["name"]) and (item["id"] == category["id"])), None)
+        if category["type"] == "movies":
+            tmp_metadata = []
+            for path, root, dirs, files in driveWalk(category["id"], False, drive):
+                for file in files:
+                    if "video" in file["mimeType"]:
+                        tmp_metadata.append(file)
+            placeholder_metadata.append({"name": category["name"], "type": category["type"], "id": category["id"],
+                                         "teamDriveId": category["teamDriveId"], "files": tmp_metadata})
+
+    metadata = placeholder_metadata
+
+    if os.path.exists("./metadata"):
+        pass
+    else:
+        os.mkdir("./metadata")
+    with open("./metadata/"+time.strftime("%Y%m%d-%H%M%S")+".json", "w+") as w:
+        w.write(json.dumps(metadata))
+
+    return metadata

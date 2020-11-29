0818c09339a540376bf6ea164ad4381aa5e75df2
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-11-29 15:53:31 +0400

    Added core data needed for the TMDB API to work

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import json
 import os
+
+import requests
 
 from src.config import readConfig, writeConfig
 from src.credentials import refreshCredentials
@@ -13,4 +16,12 @@ else:
 drive, access_token = refreshCredentials(
     "", client_id, client_secret, refresh_token, True)
 
-metadata = writeMetadata(category_list, drive)
+configuration_url = "http://api.themoviedb.org/3/configuration?api_key=" + tmdb_api_key
+configuration_content = json.loads(
+    (requests.get(configuration_url)).content)
+backdrop_base_url = configuration_content["images"]["base_url"] + \
+    configuration_content["images"]["backdrop_sizes"][3]
+poster_base_url = configuration_content["images"]["base_url"] + \
+    configuration_content["images"]["poster_sizes"][3]
+
+metadata = writeMetadata(category_list, drive, tmdb_api_key, backdrop_base_url, poster_base_url)

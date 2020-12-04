87d6cee63a8ed4ca6aa8740eb104ac30c9218b89
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-12-04 20:41:41 +0400

    Page cleanup

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ poster_base_url = configuration_content["images"]["base_url"] + \
     configuration_content["images"]["poster_sizes"][3]
 
 metadata = src.metadata.readMetadata(category_list)
-#metadata = src.metadata.writeMetadata(category_list, drive, tmdb_api_key,
-#                         backdrop_base_url, poster_base_url)
+metadata = src.metadata.writeMetadata(category_list, drive, tmdb_api_key,
+                                      backdrop_base_url, poster_base_url)
 
 app = flask.Flask(__name__)
 flask_cors.CORS(app)
@@ -113,7 +113,8 @@ def metadataAPI():
                     "category['files']" + "[" + r + "]")
                 index = index + 1
         if id:
-            ids = src.metadata.jsonExtract(obj=tmp_metadata, key="id", getObj=True)
+            ids = src.metadata.jsonExtract(
+                obj=tmp_metadata, key="id", getObj=True)
             for item in ids:
                 if item["id"] == id:
                     tmp_metadata = item

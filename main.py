84ad761537f39c2a2403908c951ab64c66fa7c84
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-02-15 20:10:51 +0400

    Fixed incorrect operator logic

--- main.py
+++ main.py
@@ -60,14 +60,13 @@ else:
 
 def create_app():
     app = flask.Flask(__name__, static_folder="build")
-    if not (len(metadata) > 0) and (datetime.datetime.utcnow() >= datetime.datetime.strptime(metadata[-1]["buildTime"], "%Y-%m-%d %H:%M:%S.%f") + datetime.timedelta(minutes=config["build_interval"])):
+    if (len(metadata) > 0) and (datetime.datetime.utcnow() <= datetime.datetime.strptime(metadata[-1]["buildTime"], "%Y-%m-%d %H:%M:%S.%f") + datetime.timedelta(minutes=config["build_interval"])):
         return app
     print("================  WRITING METADATA  ================")
     thread = threading.Thread(target=src.metadata.writeMetadata, args=(
         config, drive), daemon=True)
     thread.start()
     return app
-
 
 app = create_app()
 flask_cors.CORS(app)

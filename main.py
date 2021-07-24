07bbe8932c00ba1bc7bc95186a1ca275a952fb47
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-07-24 13:15:59 +0400

    Added back the logger

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import datetime
 import io
 import json
+import logging
 import os
 import threading
+import sys
 
 import apscheduler.schedulers.background
 import bs4
@@ -319,3 +321,30 @@ if __name__ == "__main__":
         threaded=True,
         debug=LIBDRIVE_DEBUG,
     )
+else:
+    if not os.path.exists("./logs"):
+        os.mkdir("./logs")
+    logs_path = os.path.abspath("./logs")
+    logs_max_files = 5
+
+    def sorted_ls(path):
+        mtime = lambda f: os.stat(os.path.join(path, f)).st_mtime
+        return list(sorted(os.listdir(path), key=mtime))
+
+    del_list = sorted_ls(logs_path)[0 : (len(sorted_ls(logs_path)) - logs_max_files)]
+    for del_file in del_list:
+        try:
+            os.remove(os.path.join(logs_path, del_file))
+        except:
+            pass
+
+    logging.basicConfig(
+        filename="./logs/%s.log"
+        % (datetime.datetime.utcnow().strftime("%Y%m%d-%H%M%S")),
+        level=logging.DEBUG,
+    )
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_logger = logging.getLogger()
+    console_logger.setLevel(logging.INFO)
+    console_logger.addHandler(console_handler)
